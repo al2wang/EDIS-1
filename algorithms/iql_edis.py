6098389cd3274b38c8f39eb891b1788bf46c15dc
gyw5131 <104330029+al2wang@users.noreply.github.com>
2024-08-12 22:53:26 -0400

    Update iql_edis.py

--- algorithms/iql_edis.py
+++ algorithms/iql_edis.py
@@ -531,7 +531,7 @@ def train(args, config=config):
     logger = Logger(log_dir, config.env, config.seed, info_str=args.comment)
     logger.log_str_object("parameters", log_dict = config.__dict__)
 
-    is_env_with_goal = config.env.startswith(ENVS_WITH_GOAL)
+    is_env_with_goal = config.env.startswith(ENVS_WITH_GOAL)    # check if goal-oriented env
     batch_size_offline = int(config.batch_size * config.offline_mixing_ratio)
     batch_size_online = config.batch_size - batch_size_offline
 
